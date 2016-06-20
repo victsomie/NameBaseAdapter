fbdf8cf03ddb5051fe9591fe3ce96da9c7df1a33
victsomie <victsomie@yahoo.com>
2016-06-20 23:20:43 +0300

    customized the output of the text views all together

--- app/src/main/java/com/example/victor/namebaseadapter/NameAdapter.java
+++ app/src/main/java/com/example/victor/namebaseadapter/NameAdapter.java
@@ -2,9 +2,11 @@ package com.example.victor.namebaseadapter;
 
 import android.content.Context;
 import android.support.design.widget.NavigationView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -48,6 +50,26 @@ public class NameAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {//Says I want a view in a specific position from the view given
+        View view  = null; //This makes our view
+
+        //Then test the condition of the View
+        if (view == null){
+            //If the is no view nake a view
+               //How to do this:
+               //Inflate a layout from a context(we passed this earlier)
+               //Parent refers to the groud from which it is part of
+            view = LayoutInflater.from(mContext).inflate(R.layout.name_view, parent, false);
+        }else {
+            view = convertView;
+        }
+        //Customize it
+        String name = mNames.get(position); //This gets the string at the given position
+        TextView nameTextView = (TextView)view.findViewById(R.id.name_view);
+        nameTextView.setText(name); //This sets our text view content to the name at that position
+
+        TextView positionTextView = (TextView)view.findViewById(R.id.position_view);
+        positionTextView.setText(String.format("I'm #%d ", (position+1))); //This sets the string to the position of that elemement
+
         return null;
     }
 
